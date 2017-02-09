fd075176290f5e0dc44064f53b434895b5ef656a
Ian Timmis <iantimmis@Ians-MacBook-Pro.local>
2017-02-09 17:47:47 -0500

    chapter 3

--- MusicApp/BandsTableViewController.swift
+++ MusicApp/BandsTableViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class BandsTableViewController: UITableViewController {
+    
+    let bandTitles = ["Nicole Atkins", "Ambulance LTD", "Sleepies", "Black Angels"]
+    let bandSubTitles = ["Tue 5/1", "Fri 5/4", "Sat 5/5", "Sun 5/6",]
+    let bandImageNames = ["thumb-nicole-atkins.png", "thumb-ambulance-ltd.png", "thumb-sleepies.png",
+                          "thumb-black-angels.png"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,23 +34,35 @@ class BandsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return bandTitles.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bandCell", for: indexPath)
 
         // Configure the cell...
-
+        
+        cell.textLabel?.text = bandTitles[indexPath.row]
+        cell.detailTextLabel?.text = bandSubTitles[indexPath.row]
+        cell.imageView?.image = UIImage(named: bandImageNames[indexPath.row])
+        
+        
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) ->
+        CGFloat {
+            return 88
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -82,14 +99,19 @@ class BandsTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "showDetail") {
+            
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                
+                let bandsDetailViewController:BandsDetailViewController = segue.destination as! BandsDetailViewController
+            }
+        }
     }
-    */
-
 }
