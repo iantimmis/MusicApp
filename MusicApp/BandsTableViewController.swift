74f0eec06a7ca031326c9773db226665b7b5b121
Ian Timmis <iantimmis@Ians-MacBook-Pro.local>
2017-02-21 18:31:42 -0500

    database connected

--- MusicApp/BandsTableViewController.swift
+++ MusicApp/BandsTableViewController.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class BandsTableViewController: UITableViewController {
     
     let bandsModel = BandsModel()
 
-    override func viewDidLoad() {
+    override func viewDidLoad() { 
         super.viewDidLoad()
-        
         bandsModel.fetch();
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let myRootRef = FIRDatabase.database().reference()
+        
+        //Write data to Firebase
+        myRootRef.setValue("Do you have data? You'll love firebase.")
+        myRootRef.observe(.value, with: {
+            snapshot in
+            print("\(snapshot.key) -> \(snapshot.value)")
+        })
+ 
     }
 
     override func didReceiveMemoryWarning() {
